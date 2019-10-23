8e81c99a9d956099cbb0ca172cb5a927394fb7f7
HoraceZhang <impressioncr@hotmail.com>
2019-10-23 15:47:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  workDir: '/home/kaka/ptp', // 工作文件夹
-  moveDir: '/home/kaka/deluge/download', // 做种文件夹
-  announce: 'http://please.passthepopcorn.me:2710/0nne4sudksjwdqkcxqeuv2815tbgqbqa/announce', // announce
+  workDir: '/home/kaka/ptp', // generating file location
+  moveDir: '/home/kaka/deluge/download', // seed location
+  announce: 'http://announce', // announce
 }
